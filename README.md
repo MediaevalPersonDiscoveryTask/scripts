a8c6b23eefebde5cb99c221a29e511d3b16af7c5
Hervé BREDIN <bredin@limsi.fr>
2016-02-23 10:38:41 +0100

    batch_process_MPG.sh

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scripts
-Place to backup useful scripts
+
+Various scripts used in preparation of MediaEval Person Discovery 2016
+
+* `batch_process_MPG.sh`
+  
+  - re-encode MPEG TS files into 1280x720 H264 files
+  - re-sample audio track into 16Khz WAV files
+
---
+++ batch_process_MPG.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#$ -N MPG
+#$ -S /bin/bash
+#$ -o /vol/work1/bredin/mediaeval/PersonDiscovery2016/logs
+#$ -e /vol/work1/bredin/mediaeval/PersonDiscovery2016/logs
+#$ -v LD_LIBRARY_PATH
+#$ -v PATH
+
+ORIGINAL="/vol/corpora5/mediaeval/INA/LAffaireSnowden"
+PROCESSED="/vol/work1/bredin/mediaeval/PersonDiscovery2016/INA/LAffaireSnowden"
+
+if [ -n "${SGE_TASK_ID+x}" ]; then
+
+    STEP=${SGE_TASK_LAST}-${SGE_TASK_FIRST}+1
+    FIRST=${SGE_TASK_ID}-${SGE_TASK_FIRST}+1
+
+    LIST=$1
+    
+    NUMBER=`wc -l $LIST|awk '{print $1}'`
+
+    for (( f=$FIRST; f<=$NUMBER; f=f+$STEP ))
+    do
+
+      RELATIVE=$(head -n ${f} ${LIST} |tail -n 1)
+      
+      INPUT=$ORIGINAL/$RELATIVE
+      OUTPUT=$PROCESSED/$RELATIVE.mp4
+      mkdir -p `dirname $OUTPUT`
+      ffmpeg -y -i $ORIGINAL/$RELATIVE -vcodec libx264 -preset medium -s 1280x720 -acodec copy $PROCESSED/$RELATIVE.mp4
+      ffmpeg -y -i $PROCESSED/$RELATIVE.mp4 -ac 1 $PROCESSED/$RELATIVE.raw.wav
+      sndfile-resample -to 16000 -c 0 $PROCESSED/$RELATIVE.raw.wav $PROCESSED/$RELATIVE.wav
+      rm $PROCESSED/$RELATIVE.raw.wav
+
+    done
+else
+    echo "What? ffmpeg -i $ORIGINAL/{VIDEO} -vcodec libx264 -preset medium -s 1280x720 -acodec copy $PROCESSED/{VIDEO}.mp4"
+    echo "      for each {VIDEO} in file 'video_list'"
+    echo "How? qsub -t 1-NSPLIT `basename $0` video_list"
+fi
+
